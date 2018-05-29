1deb357ce574800a025603b4f9600f9eaa9ee0f4
jiang <995868061@qq.com>
2018-05-29 23:58:58 +0800

    修改参数

--- src/main/java/com/even/sell/controller/SellerUserController.java
+++ src/main/java/com/even/sell/controller/SellerUserController.java
@@ -1,27 +1,30 @@
 package com.even.sell.controller;
 
+import com.even.sell.VO.ResultVO;
 import com.even.sell.constant.CookieConstant;
 import com.even.sell.dataobject.SellerInfo;
 import com.even.sell.enums.ResultEnum;
+import com.even.sell.repository.SellerInfoRepository;
 import com.even.sell.service.SellerService;
 import com.even.sell.utils.CookieUtil;
+import com.even.sell.utils.ResultVOUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-import static com.even.sell.constant.CookieConstant.EXPIRE;
-
 /**
  * 卖家用户
+ *
  * @author EVEN
  * 2018/5/1 19:29
  */
@@ -32,6 +35,9 @@ public class SellerUserController {
     @Autowired
     private SellerService sellerService;
 
+    @Autowired
+    private SellerInfoRepository sellerInfoRepository;
+
 //    @Autowired
 //    private StringRedisTemplate redisTemplate;
 
@@ -39,31 +45,41 @@ public class SellerUserController {
 //    private ProjectUrlConfig projectUrlConfig;
 
     @GetMapping("/login")
-    public ModelAndView login(@RequestParam("openid") String openid,
-                              HttpServletResponse response,
-                              Map<String, Object> map) {
+    @ResponseBody
+    public ResultVO login(
+            HttpServletResponse response) {
 
         //1. openid去和数据库里的数据匹配
-        SellerInfo sellerInfo = sellerService.findSellerInfoByOpenid("123123");
-        if (sellerInfo == null) {
-            map.put("msg", ResultEnum.LOGIN_FAIL.getMessage());
-            map.put("url", "/sell/seller/order/list");
-            return new ModelAndView("common/error");
-        }
-
-        //2. 设置token至redis
-        String token = UUID.randomUUID().toString();
-//        Integer expire = RedisConstant.EXPIRE;
+        SellerInfo sellerInfo = sellerInfoRepository.findByUsernameAndPassword("慕善文", "123456");
+//        if (sellerInfo == null) {
+//            map.put("msg", ResultEnum.LOGIN_FAIL.getMessage());
+//            map.put("url", "/sell/seller/order/list");
+//            return new ModelAndView("common/error");
+//        }
 //
-//        redisTemplate.opsForValue().set(String.format(RedisConstant.TOKEN_PREFIX, token), openid, expire, TimeUnit.SECONDS);
-
-        //3. 设置token至cookie
-        CookieUtil.set(response, CookieConstant.TOKEN, token, EXPIRE);
-
-        //return new ModelAndView("redirect:" + projectUrlConfig.getSell() + "/sell/seller/order/list");
-        return new ModelAndView("redirect:" + "/sell/seller/order/list");
+//        //2. 设置token至redis
+        String token = UUID.randomUUID().toString();
+////        Integer expire = RedisConstant.EXPIRE;
+////
+////        redisTemplate.opsForValue().set(String.format(RedisConstant.TOKEN_PREFIX, token), openid, expire, TimeUnit.SECONDS);
+//
+//        //3. 设置token至cookie
+//        CookieUtil.set(response, CookieConstant.TOKEN, token, EXPIRE);
+        return sellerInfo!=null ? ResultVOUtil.success(sellerInfo) : ResultVOUtil.error(50000, "失败");
+//        //return new ModelAndView("redirect:" + projectUrlConfig.getSell() + "/sell/seller/order/list");
+//        return new ModelAndView("redirect:" + "/sell/seller/order/list");
 
     }
+
+    @GetMapping("/findAll")
+    @ResponseBody
+    public ResultVO findAll(HttpServletResponse response) {
+
+        //1. openid去和数据库里的数据匹配
+        List<SellerInfo> sellerInfoList = sellerInfoRepository.findAll();
+        return sellerInfoList.size() > 0 ? ResultVOUtil.success(sellerInfoList) : ResultVOUtil.error(50000, "失败");
+    }
+
 
     @GetMapping("/logout")
     public ModelAndView logout(HttpServletRequest request,
--- src/main/java/com/even/sell/repository/SellerInfoRepository.java
+++ src/main/java/com/even/sell/repository/SellerInfoRepository.java
@@ -9,4 +9,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface SellerInfoRepository extends JpaRepository<SellerInfo, String> {
     SellerInfo findByOpenid(String openid);
+
+    SellerInfo findByUsernameAndPassword(String username, String password);
 }
