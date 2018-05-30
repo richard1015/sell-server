196f565e8a6520cbf3d6ae0d3dc01d9ae1477e76
jiang <995868061@qq.com>
2018-05-30 14:21:50 +0800

    questionController

--- src/main/java/com/even/sell/controller/QuestionController.java
+++ src/main/java/com/even/sell/controller/QuestionController.java
@@ -1,30 +1,19 @@
 package com.even.sell.controller;
 
 import com.even.sell.VO.ResultVO;
-import com.even.sell.VO.UserVO;
-import com.even.sell.dataobject.BuyerInfo;
 import com.even.sell.dataobject.Question;
-import com.even.sell.enums.ResultEnum;
-import com.even.sell.form.LoginForm;
-import com.even.sell.repository.BuyerInfoRepository;
 import com.even.sell.repository.QuestionRepository;
 import com.even.sell.service.BuyerService;
 import com.even.sell.utils.CookieUtil;
 import com.even.sell.utils.ResultVOUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Pageable;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
-
 import java.util.Date;
 import java.util.List;
-
-import static com.even.sell.constant.CookieConstant.EXPIRE;
 
 
 /**
@@ -41,50 +30,57 @@ public class QuestionController {
 
     @Autowired
     private QuestionRepository questionRepository;
+    @Autowired
+    private BuyerService buyerService;
 
     /**
      * 保存问题
      *
-     * @param  question
+     * @param question
      * @return
      */
     @PostMapping("/insert")
-    public ResultVO insert(@Valid @RequestBody Question question) {
+    public ResultVO insert(HttpServletRequest request,@Valid @RequestBody Question question) {
+        Cookie cookie = CookieUtil.get(request, "userId");
+        String buyerId = buyerService.findBuyerInfoById(cookie.getValue()).getId();
         question.setCreateTime(new Date());
         question.setStatus(0);
-       Question question1 = questionRepository.save(question);
-       return question1 != null ? ResultVOUtil.success() : ResultVOUtil.error(50000, "保存失败");
+        question.setBuyerId(buyerId);
+        Question question1 = questionRepository.save(question);
+        return question1 != null ? ResultVOUtil.success() : ResultVOUtil.error(50000, "保存失败");
     }
 
     /**
      * 卖家回复问题
      *
-     * @param  question
+     * @param question
      * @return
      */
     @PostMapping("/reply")
     public ResultVO reply(@Valid @RequestBody Question question) {
         question.setUpdateTime(new Date());
         question.setStatus(1);
-        int codeRes = questionRepository.updateById(question.getSellerId(),question.getReply(),question.getStatus(),question.getUpdateTime(),question.getId());
-        return codeRes >0 ? ResultVOUtil.success() : ResultVOUtil.error(50000, "回复失败");
+        int codeRes = questionRepository.updateById(question.getSellerId(), question.getReply(), question.getStatus(), question.getUpdateTime(), question.getId());
+        return codeRes > 0 ? ResultVOUtil.success() : ResultVOUtil.error(50000, "回复失败");
     }
 
     /**
      * 根据买家id查询问题
      *
-     * @param  question
+     * @param
      * @return
      */
     @PostMapping("/findAllByBuyerId")
-    public ResultVO findAllByBuyerId(@Valid @RequestBody Question question) {
-        question.setCreateTime(new Date());
-        List<Question> questionList = questionRepository.findByBuyerId(question.getBuyerId());
-        return questionList.size() >0 ? ResultVOUtil.success(questionList) : ResultVOUtil.error(50000, "回复失败");
+    public ResultVO findAllByBuyerId(HttpServletRequest request) {
+        Cookie cookie = CookieUtil.get(request, "userId");
+        String buyerId = buyerService.findBuyerInfoById(cookie.getValue()).getId();
+        List<Question> questionList = questionRepository.findByBuyerId(buyerId);
+        return questionList.size() > 0 ? ResultVOUtil.success(questionList) : ResultVOUtil.error(50000, "回复失败");
     }
 
     /**
      * \根据卖家Id进行查询问题
+     *
      * @param question
      * @return
      */
@@ -92,9 +88,8 @@ public class QuestionController {
     public ResultVO findAllBySellerId(@Valid @RequestBody Question question) {
         question.setCreateTime(new Date());
         List<Question> questionList = questionRepository.findBySellerId(question.getSellerId());
-        return questionList.size() >0 ? ResultVOUtil.success(questionList) : ResultVOUtil.error(50000, "回复失败");
+        return questionList.size() > 0 ? ResultVOUtil.success(questionList) : ResultVOUtil.error(50000, "回复失败");
     }
-
 
 
 }
